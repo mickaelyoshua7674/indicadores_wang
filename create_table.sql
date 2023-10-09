3acf5bf46cb00c091fc2909eb760e1f82915e9fc
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-10-09 09:21:57 -0300

    define lenght of VARCHAR fields

--- create_table.sql
+++ create_table.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS dbo.valores_finbra;
 CREATE TABLE dbo.valores_finbra (
     Ano INT,
-    Tipo VARCHAR(300),
-    Instituição VARCHAR(300),
+    Tipo VARCHAR(50),
+    Instituição VARCHAR(74),
     IBGE INT,
-    UF VARCHAR(300),
+    UF VARCHAR(3),
     População INT,
-    Coluna DATE,
-    Conta VARCHAR(300),
-    Descrição Conta VARCHAR(300),
-    Identificador da Conta VARCHAR(300),
+    Coluna VARCHAR(96),
+    Conta VARCHAR(160),
+    Descrição Conta VARCHAR(213),
+    Identificador da Conta VARCHAR(86),
     Valor FLOAT
 );
